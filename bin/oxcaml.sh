74e44c15eb1fe9a7496af38389d54a1778d68351
Anil Madhavapeddy <anil@recoil.org>
2024-11-09 17:36:19 +0000

    more

--- bin/oxcaml.sh
+++ bin/oxcaml.sh
@@ -9,7 +9,7 @@ if [[ ! -d "${ROOT}" ]]
 then
   echo Initialising opam for OxCaml
   mkdir -p "${ROOT}"
-  opam init -n --bare
+  opam init -n --bare --disable-sandboxing
   opam switch create oxcaml --empty
   opam repo add with-extensions "https://github.com/janestreet/opam-repository.git#with-extensions"
   opam install -y ocaml-variants.5.2.0+flambda2 ocaml-lsp-server
