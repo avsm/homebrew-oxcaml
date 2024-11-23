b2feb6892026d457cbf9b0848a5aa4f3fd9734c4
Anil Madhavapeddy <anil@recoil.org>
2024-11-23 11:09:59 +0000

    dune: update 3.17+dev-2 bottle.

--- Formula/dune.rb
+++ Formula/dune.rb
@@ -6,9 +6,9 @@ class Dune < Formula
   license "LGPL-2.1-only" => { with: "OCaml-LGPL-linking-exception" }
 
   bottle do
-    root_url "https://github.com/avsm/homebrew-oxcaml/releases/download/dune-3.17+dev"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "322901b53dbfdbe442e6e1493cc93a036e1f3fc84a696adf9ff7d9125703c383"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4f0389e68db4e2d72f47afbe03adc7a5d20556db467b41ab5695657134adca20"
+    root_url "https://github.com/avsm/homebrew-oxcaml/releases/download/dune-3.17+dev-2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fbfbed7c1db67175eee795863b1c526dcf70a26e66900fe21a4a184343fe3c48"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8bd1abaa8d5a767aa51e8e53f5f40ecfbdd3070b34ce81eb4a1c00477dd58daa"
   end
 
   depends_on "ocaml"
